52a4c267d64e7e61f7a7f3fc766ef9f76fa3d5fc
Amy Burka <aburka@gmail.com>
2018-07-10 14:25:44 -0400

    replace config class with default config constant

--- lib/nstance/rails.rb
+++ lib/nstance/rails.rb
@@ -1,12 +1,21 @@
 require "nstance/rails/engine"
-require "nstance/rails/config"
 require "nstance/rails/token"
 require "nstance/rails/eval_instance"
 
 module Nstance
   module Rails
+    # Default used if constant not set in config/initializers/nstance_rails.rb
+    LANGUAGE_CONFIGS = {
+      "javascript" => { cmd: "node",          image: "node:6.2.0" },
+      "ruby"       => { cmd: "ruby",          image: "ruby:2.4.0" },
+      "python"     => { cmd: "python",        image: "python:3.5.1" },
+      "swift"      => { cmd: "swift",         image: "swiftdocker/swift:snapshot-2016-09-02-a" },
+      "java"       => { cmd: "",              image: "openjdk:8-jdk" },
+      "csharp"     => { cmd: "/app/build.sh", image: "jwolgamott/tiy-dotnet-runnable:0.4-beta" }
+    }
+
     def self.image_for_lang(lang)
-      config = Nstance::Rails::Config.config_for_lang(lang)
+      config = Nstance::Rails::LANGUAGE_CONFIGS[lang]
       config[:image]
     end
 
